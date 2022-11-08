5026809aab1cb1adcac99692ab50c1f1c3038f47
Francis Barrow <francis.barrow@gmail.com>
2022-11-08 17:41:13 -0500

    add ID to Player

--- Player.py
+++ Player.py
@@ -1,11 +1,15 @@
 class Player:
     def __init__(self, first_name, last_name, date_of_birth,
-                 gender, ranking):
+                 gender, ranking, player_id=None):
         self.first_name = first_name
         self.last_name = last_name
         self.date_of_birth = date_of_birth
         self.gender = gender
         self.ranking = ranking
+        if player_id is None:
+            self.id = self.__hash__()
+        else:
+            self.id = player_id
 
     def get_full_name(self):
         return self.first_name + ' ' + self.last_name
