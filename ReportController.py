575bac03fe1a122811979d8af5baf1dbf8707719
Francis Barrow <francis.barrow@gmail.com>
2022-11-10 14:36:01 -0500

    Move tournament reports to SingleTournamentController

--- ReportController.py
+++ ReportController.py
@@ -5,7 +5,7 @@ from Option import Option
 class ReportController:
     def __init__(self, players_control, tournaments_control):
         self.players_control = players_control
-        self.tournaments = tournaments_control.tournaments
+        self.tournaments_control = tournaments_control
         self.base_view = BaseView()
 
     def run_reports(self):
@@ -26,40 +26,17 @@ class ReportController:
                    Option('Exit report menu', self.exit)]
         return options
 
-    # def report_players(self, players):
-    #     if players is None:
-    #         players = self.players_control.players
-    #     if len(players) == 0:
-    #         self.view.notice_no_players_to_show()
-    #     else:
-    #         if self.view.prompt_for_order_preference(['ranking', 'alphabetical']) == 0:
-    #             players = play.order_by_ranking(players)
-    #         else:
-    #             players = self.order_alphabetically(players)
-    #         # either:
-    #         self.view.show_players(players)
-    #         # or, if players is a list of SinglePlayerControllers:
-    #         for player in players:
-    #             player.display_player()
-    #
-    # def order_by_ranking(self, players):
-    #     players.sort(key=lambda x: x.ranking, reverse=True)
-    #     return players
-    #
-    # def order_alphabetically(self, players):
-    #     players.sort(key=lambda x: (x.last_name, x.first_name))
-    #     return players
-
-    def report_tournaments(self, tournaments=None):
-        if tournaments is None:
-            tournaments = self.tournaments
+    def report_tournaments(self):
+        tournaments = self.tournaments_control.tournaments
         if len(tournaments) == 0:
             # self.view.notice_no_tournaments_to_show()
             pass
         else:
             options = []
             for tournament in tournaments:
-                options.append(Option(tournament.name, ))
+                options.append(Option(tournament.tournament.name, tournament.run_reports))
+            options.append(Option('Back home', self.exit))
+            self.generic_report_menu(options)
 
     def exit(self):
         pass
