e9ebda60f616c168a3ee2f8d73326c250616fa40
Francis Barrow <francis.barrow@gmail.com>
2022-11-11 18:22:36 -0500

    add Message

---
+++ Models/Message.py
@@ -0,0 +1,39 @@
+class Message:
+    #
+    # Greeting and Navigation Messages
+    #
+    PROGRAM_NAME = 'Castle Chess Tournament Manager'
+    with open('CastleChessLogo-ascii-art.txt') as f:
+        LOGO_ASCII = f.read()
+    WELCOME = '~ Welcome! ~'
+    START_PROGRAM = 'Press Enter to start...'
+    GOOD_BYE = 'Thank you for playing! See you next time!'
+
+    #
+    # Titles
+    #
+    MAIN_MENU = PROGRAM_NAME
+    ONGOING_TOURNAMENT_MENU = 'Tournament'
+    ARCHIVED_TOURNAMENT_MENU = 'Archived Tournaments'
+    REPORT_MENU = 'Reports'
+    TOURNAMENTS_TITLE = 'Tournaments'
+    ROUNDS_TITLE = 'Rounds'
+    PLAYERS_TITLE = 'Players'
+    STARTING = 'Starting '
+    ENDING = 'Ending '
+    EDIT_PLAYERS_MENU = "Edit Players"
+
+    #
+    # Menu Options
+    #
+    ALPHABETICAL = 'Alphabetical'
+    RANKING = 'Ranking'
+    EXIT_MENU = 'Exit far away'
+
+    #
+    # Notices
+    #
+    NO_MORE_PAIRINGS = 'All the players payed against each other, there is no more match to play in this tournament.'
+    TOURNAMENT_OVER = 'The tournament is now over.'
+    NO_TOURNAMENTS = 'No tournaments to show.'
+    NO_PLAYERS = 'No players to show.'
